d35ec38b880601336850bc9227c405ab97b28925
Dexter Whelan <whelandexter@gmail.com>
2025-03-27 12:45:58 +0000

    building an extra page while reusing components was super quick. much faster than expected

--- src/Pages/Episodes.js
+++ src/Pages/Episodes.js
@@ -1,9 +1,11 @@
 import React,{useState, useEffect} from 'react'
+import Cards from '../components/Cards/Cards';
+import InputGroup from '../components/Filters/Category/InputGroup';
 
 const Episodes = () => {
   let[id, setID] = useState(1);
   let[info, setInfo] = useState([]);
-  let[results, setResults] = useState();
+  let[results, setResults] = useState([]);
   let {air_date, name} = info
   let api = `https://rickandmortyapi.com/api/episode/${id}`;
 
@@ -11,13 +13,24 @@ const Episodes = () => {
     (async function(){
       let data = await fetch(api).then(res=>res.json());
       setInfo(data);
-      let a = a;
+      let a = await Promise.all(
+        data.characters.map((x) => {
+          /* in here we have the characters aspect of the data object. 
+          then we can map the data into a for each function. inside the brackets
+          we create the function with ()=>{} so the result gets passed back 
+          to the map function. here i suspect we use some way of fetching 
+          data with api that gets broken out here. 
+          */
+          return fetch(x).then((res) => res.json());
+        })
+      );
+      setResults(a);
     })()
   },[api]);
 
   return (
     <div className='container'>
-      <div className="row">
+      <div className="row mb-4">
         <h1 className="text-center mb-3 ">
           Episode : {" "}
           <span className="text-primary"> 
@@ -30,11 +43,15 @@ const Episodes = () => {
       </div>
       <div className="row">
         <div className="col-3">
-          Pick episodes
+          <h4 className="text-center mb-4">
+            Pick episodes
+          </h4>
+          <InputGroup setID={setID} name="Episode" total={51}/>
+          
         </div>
         <div className="col-8">
           <div className="row">
-          cards are here
+            <Cards results={results}/>
           </div>
         </div>
       </div>
